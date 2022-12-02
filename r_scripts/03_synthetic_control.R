b57f14ce314dad54118164b43a3feaa43b778f24
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2022-12-02 11:02:18 +0100

    rename object

--- r_scripts/03_synthetic_control.R
+++ r_scripts/03_synthetic_control.R
@@ -5,7 +5,7 @@ prop99 <- read_rds("data/proposition99.rds")
 
 
 # Create synthetic control object
-syncont <- 
+prop99_syn <- 
   prop99 |> 
   synthetic_control(
     outcome = cigsale,
@@ -18,12 +18,12 @@ syncont <-
 
 # in tidysynth, the grab_* functions can be used to inspect parts of the model
 # here, we inspect the outcome (cigsale) for the treated unit and the potential controls
-grab_outcome(syncont)
-grab_outcome(syncont, type = "controls")
+grab_outcome(prop99_syn)
+grab_outcome(prop99_syn, type = "controls")
 
 # Now, generate the aggregate predictors used to estimate the weights
-syncont <- 
-  syncont |> 
+prop99_syn <- 
+  prop99_syn |> 
   generate_predictor(
     time_window = 1980:1988,
     lnincome = mean(lnincome, na.rm = TRUE),
@@ -48,29 +48,29 @@ syncont <-
   )
 
 # Check the predictors we created for the treated and the control units
-grab_predictors(syncont)
-grab_predictors(syncont, type = "controls")
+grab_predictors(prop99_syn)
+grab_predictors(prop99_syn, type = "controls")
 
 
 # Then, we can create our weights matrix
 # this uses a quadratic programming routine (ipop) for optimization
-syncont <- 
-  syncont |> 
+prop99_syn <- 
+  prop99_syn |> 
   generate_weights(
     optimization_window = 1970:1988, # pre-intervention period
     margin_ipop = 0.2, sigf_ipo = 7, bound_ipop = 6
   )
 
 # let's look at the learned unit weights and predictor weights
-grab_unit_weights(syncont)
-grab_predictor_weights(syncont)
+grab_unit_weights(prop99_syn)
+grab_predictor_weights(prop99_syn)
 
 # and we can create a plot as well
-plot_weights(syncont)
+plot_weights(prop99_syn)
 
 
 # Generate the synthetic control
-syncont_control <- generate_control(syncont)
+prop99_syn_control <- generate_control(prop99_syn)
 
-plot_trends(syncont_control, time_window = 1970:2000) 
-plot_placebos(syncont_control, time_window = 1970:2000)
+plot_trends(prop99_syn_control) 
+plot_placebos(prop99_syn_control)
